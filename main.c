e73e3c265d60e62bb4a83744396000340faf48f7
Kyile Woods <44454180+KyileWoods@users.noreply.github.com>
2019-05-31 13:03:16 +1000

    Update main.c

--- main.c
+++ main.c
@@ -8,46 +8,45 @@
 
 
 
-void timer0_init() {
-    //set up timer0 to output fast PWM, 256 prescaler, clear on compare match, set on top
-    DDRB |= (1 << 7);
-    DDRD |= (1 << 0);
-    TCCR0A |= (1 << 7) | (1 << 5) | (1 << 1) | 1;
-    TIMSK0 |= (1 << 0); //enable overflow interrupt
-    OCR0B = 0;
-    TCCR0B |= (1 << 2);
+void timer0_init(){
+	//set up timer0 to output fast PWM, 256 prescaler, clear on compare match, set on top
+	DDRB |= (1<<7);
+	DDRD |= (1<<0);
+	TCCR0A |= (1<<7)|(1<<5)|(1<<1)|1;
+	TIMSK0 |= (1<<0); //enable overflow interrupt
+	OCR0B = 0;
+	TCCR0B |= (1<<2);
 }
 
 
 
-void timer1_init() {
-    //set up timer1 to output fast PWM (8-bit), 256 prescaler, clear on compare match, set on top
-    DDRB |= (1 << 6) | (1 << 5);
-    TCCR1A |= (1 << 7) | (1 << 5) | 1;
-    TCCR1B |= (1 << 3);
-    TIMSK1 |= (1 << 0); ////enable overflow interrupt
-    OCR1B = 0;
-    TCCR1B |= (1 << 2);
+void timer1_init(){
+	//set up timer1 to output fast PWM (8-bit), 256 prescaler, clear on compare match, set on top
+	DDRB |= (1<<6)|(1<<5);
+	TCCR1A |= (1<<7)|(1<<5)|1;
+	TCCR1B |= (1<<3);
+	TIMSK1 |= (1<<0); ////enable overflow interrupt
+	OCR1B = 0;
+	TCCR1B |= (1<<2);
 }
 
 
 
-void ADC_init() {
+void ADC_trim_init(){
     ADMUX = (0x20); //Sets the reference to the internal 2.56V and left adjusts the results
     ADCSRA = (0x87); // Enables the ADC conversion and sets the prescaler to 128
-    DDRB |= (1 << 1); //This and the next line turns on the IR LED of the sensor board.//This code isn't actually needed i don't think
-    DDRB |= (1 << 3); //because CHris has the VCC directly connected to sensors. I left it in because it didn't seem to create any conflict.
+   DDRB |= (1<<0);DDRB |= (1<<1);DDRB |= (1<<2); //This and the next line turns on the IR LED of the sensor board.//This code isn't actually needed i don't think
 }
 
 
 
 // Reads ADC4 which is connected to sensor 4 on the new board. 4rd line sensing LED from the LHS looking from the back
-int adc_read4() {
-    ADCSRB = (ADCSRB & 0b11011111); //clears MUX5
+int adc_read4 (){
+    ADCSRB =(ADCSRB & 0b11011111); //clears MUX5
     ADMUX = (ADMUX & 0xF8); //clears the botom 3 bits of the ADMUX register before oring the channel to be converted
-    ADMUX |= (0b00000100);
-    ADCSRA |= (1 << 6);   // sets a flag to start a single conversion of the value on ch ADC channel
-    while (ADCSRA  & (1 << 6)) {}
+    ADMUX |=(0b00000100);
+    ADCSRA |= (1<<6);   // sets a flag to start a single conversion of the value on ch ADC channel
+    while (ADCSRA  & (1<<6)){}
     return ADCH;
 
 }
@@ -55,12 +54,12 @@ int adc_read4() {
 
 
 // Reads ADC5 which is connected to sensor 3 on the new board. 3rd line sensing LED from the LHS looking from the back
-int adc_read3() {
-    ADCSRB = (ADCSRB & 0b11011111); //clears MUX5
+int adc_read3 (){
+    ADCSRB =(ADCSRB & 0b11011111); //clears MUX5
     ADMUX = (ADMUX & 0xF8); //clears the botom 3 bits of the ADMUX register before oring the channel to be converted
-    ADMUX |= (0b00000101);
-    ADCSRA |= (1 << 6);   // sets a flag to start a single conversion of the value on ch ADC channel
-    while (ADCSRA  & (1 << 6)) {}
+    ADMUX |=(0b00000101);
+    ADCSRA |= (1<<6);   // sets a flag to start a single conversion of the value on ch ADC channel
+    while (ADCSRA  & (1<<6)){}
     return ADCH;
 
 }
@@ -69,12 +68,12 @@ int adc_read3() {
 
 // Reads ADC6 which is connected to sensor 2 on the new board. 2nd line detecting LED from the LHS from the back
 
-int adc_read2() {
-    ADCSRB = (ADCSRB & 0b11011111); //clears MUX5
+int adc_read2 (){
+    ADCSRB =(ADCSRB & 0b11011111); //clears MUX5
     ADMUX = (ADMUX & 0xF8); //clears the botom 3 bits of the ADMUX register before oring the channel to be converted
-    ADMUX |= (0b00000110);
-    ADCSRA |= (1 << 6);   // sets a flag to start a single conversion of the value on ch ADC channel
-    while (ADCSRA  & (1 << 6)) {}
+    ADMUX |=(0b00000110);
+    ADCSRA |= (1<<6);   // sets a flag to start a single conversion of the value on ch ADC channel
+    while (ADCSRA  & (1<<6)){}
     return ADCH;
 }
 
@@ -82,12 +81,12 @@ int adc_read2() {
 
 // Reads ADC7 which is connected to the RHS side marker LED
 
-int RHS_sidemarkerADC() {
-    ADCSRB = (ADCSRB & 0b11011111); //clears MUX5
+int RHS_sidemarkerADC (){
+    ADCSRB =(ADCSRB & 0b11011111); //clears MUX5
     ADMUX = (ADMUX & 0xF8); //clears the botom 3 bits of the ADMUX register before oring the channel to be converted
-    ADMUX |= (0b00000111);
-    ADCSRA |= (1 << 6);   // sets a flag to start a single conversion of the value on ch ADC channel
-    while (ADCSRA  & (1 << 6)) {}
+    ADMUX |=(0b00000111);
+    ADCSRA |= (1<<6);   // sets a flag to start a single conversion of the value on ch ADC channel
+    while (ADCSRA  & (1<<6)){}
     return ADCH;
 }
 
@@ -95,12 +94,12 @@ int RHS_sidemarkerADC() {
 
 // Reads ADC11 which is connected to sensor 1 on the new board. That is the first line detecting LED on the LHS looking from the back
 
-int adc_read1() {
-    ADCSRB |= (0b00100000); //Sets MUX5
+int adc_read1 (){
+    ADCSRB |=(0b00100000); //Sets MUX5
     ADMUX = (ADMUX & 0xF8); //clears the botom 3 bits of the ADMUX register before oring the channel to be converted
-    ADMUX |= (0b00000011);
-    ADCSRA |= (1 << 6);   // sets a flag to start a single conversion of the value on ch ADC channel
-    while (ADCSRA  & (1 << 6)) {}
+    ADMUX |=(0b00000011);
+    ADCSRA |= (1<<6);   // sets a flag to start a single conversion of the value on ch ADC channel
+    while (ADCSRA  & (1<<6)){}
     return ADCH;
 }
 
@@ -108,155 +107,172 @@ int adc_read1() {
 
 // Reads ADC10 which is connected to the LHS side marker LED
 
-int LHS_sidemarkerADC() {
+int LHS_sidemarkerADC (){
     ADMUX = (ADMUX & 0xF8); //clears the botom 3 bits of the ADMUX register before oring the channel to be converted
-    ADMUX |= (0b00000010);
-    ADCSRB |= (0b00100000); //Sets MUX5
-    ADCSRA |= (1 << 6);   // sets a flag to start a single conversion of the value on ch ADC channel
-    while (ADCSRA  & (1 << 6)) {}
+    ADMUX |=(0b00000010);
+    ADCSRB |=(0b00100000); //Sets MUX5
+    ADCSRA |= (1<<6);   // sets a flag to start a single conversion of the value on ch ADC channel
+    while (ADCSRA  & (1<<6)){}
     return ADCH;
 }
 
-int error_value[10]; int corner;
-
-int corner_detect(int error, int speed) {
-    /*
-    Counts to 12 overflow events
-    */
-
-    corner++; //Timer overflows 500 times in a second count 12 overflows to get 0.024s
-    if (corner > 12) {
-        corner = 0; int error_average = 0; int i, x; //resets our timer count and average
-         // For loop shuffles the average values down one spot in the aray to clear a spot for the new value
-        for (i = 9; i > 0; i--) {
-            error_value[i] = error_value[i - 1];
-        }
-        error_value[0] = error; //Stores the latest error value
-        for (x = 0; x < 10; x++) {
-            error_average += error_value[x]; //Sums the stored PID values
-        }
-        error_average = error_average / 9; //Gets the average
-        if (error_average > 2 || error_average < -2) { //The values in this if statement set the value that the average needs to be within to say it is on a straight.
-            speed = 20;// gain = 100; Kd = 50;                   //Speed and PD values if it is deemed the robot is on a corner
-            PORTB = (PORTB & 0b11111101);   //Turns the LED off if is on a corner
-        }
-        else {
-            PORTB |= (1 << 1);     //IF it is on a Straight turn the LED on.
-            speed = 30;//gain = 100;Kd = 50;  //SPeeds and PD values for the straight.
-        }
-    }
-    return speed;
-}
-
-
-void count_down() {
-    DDRB |= (1 << 0) | (1 << 1) | (1 << 2);
-    DDRE |= (1 << 6);
-    PORTB |= (1 << 0) | (1 << 1) | (1 << 2);
-    PORTE |= (1 << 6);
-
-    _delay_ms(1000);
-    PORTE ^= (1 << 6);
-    for (int i = 0; i <= 2; i++) {
-        _delay_ms(700);
-        PORTB ^= (1 << i);
-    }
-    
-
-} 
-
-
-void setup() {
+void setup(){
     timer0_init();
     timer1_init();
-    ADC_init();
-    count_down();
+    ADC_trim_init();
 }
+//int corner_on;
 
 
+// int side_markercheck(int corner_on){
+//     int value = LHS_sidemarkerADC(); //Reads from D7 which is the right sensor on the new board.
+//     if(value >= 100 &&corner_on==0){ //Checks to see if it is on a white marker
+//         PORTB |=(1<<0); //Turns on an LED1
+//         //corner_on = 0; //Some memory to know that it has been switched on.
+//         return 1; //Return 1 so we can count
+//     }else if(value<100 && corner_on == 3) {
+//         PORTB = (PORTB & 0b11111110);
+//         //corner_on = 1;
+//         return 0;
+//     }else{
+//         return 3;
+//     }
+// }
 
-int cautious_turning(int LED_2, int LED_3) {
-    long int diff; int speed;
-    diff = (LED_3 * 1000) / LED_2;
-    if (diff<2050 && diff>-50) { //??? How would a negative value come about? Perhaps not important, but I don't think 'diff>= -50' will ever be false.
-        speed = 35;
-        PORTB |= (1 << 1);
-    }
-    else {
-        speed = 19;
-        PORTB = (PORTB & 0b11111101);
-    }
-    if (LHS_sidemarkerADC() > 180) {
-        speed = 18;
+
+//This is the code to speed it up on the straights. In reality it doesn't work exactly work like that. It does speed up if it is going straight enough
+int corner_2 (int LED_2,int LED_3){
+    long int diff;int speed;
+    diff = (LED_3*1000)/LED_2;
+    if(diff<500&&diff>-50){
+    speed = 37;    
+    
+        PORTB |=(1<<1);
+    }else{
+    speed = 18;
+    PORTB = (PORTB & 0b11111101);
+        }
+    if (LHS_sidemarkerADC()>180){
+        speed = 12;
     }
     return speed;
 }
 
-long int run_PID(int prev_error, long int integral) {
-    int Kp = 30;  // this equals a gain of 0.5 all gains need to be multiplied by 1000
-    int Kd = 5; // This equals a Kp value of 0.5 same as above //
-    int Ki = 10
-    long int error; int PID; long int kd_factor; int ki_factor; int kp_factor
-    
-    //Find the error
-    long int LED1 = adc_read1(); int LED2 = adc_read2(); int LED3 = adc_read3(); int LED4 = adc_read4(); 
-    int Sum_LED = LED1 + LED2 + LED3 + LED4;
-    long int weighted_error = ((LED1)+(LED2 * 2) + (LED3 * 3) + (LED4 * 4)) * 1000;
-    error = (weighted_error / Sum_LED) - 2500;
-
-    kp_factor
-
-    if(integral<0 && (integral+error)>0){
-        integral = 0;
-    }
-    elseif(integral>0 && (integral+error)<0)
-    
-    integral = integral + error;
-    ki_factor = integral/ki_factor;
-
-    kd_factor = (Kd *((error)-(prev_error) / 100)); //This calculates the derivative side of the PD
-    
-    PID = (kp_factor + kd_factor + ki_factor) / 1000; // Calculates the PID value.
-    
-    // Check if the robot may be a little too far out of alignment. Helps to slow down and drive carefully.
-    int speed = cautious_turning(LED2, LED3);
-    if (PID > speed) {
-        PID = speed;
-    }
-    if (PID < -speed) {
-        PID = -speed;
-    }
-
-    OCR0A = (speed + (-PID)); //this drives motor 2 or the left wheel
-    OCR1A = (speed + PID); //this drives motor 1 or the right wheel
-    _delay_ms(5); //Delay for the derivative function
-    return error;
+long int run_PID (int prev_error){
+        int gain = 75;  // this equals a gain of 0.5 all gains need to be multiplied by 1000
+        int Kd = 4; // This equals a Kp value of 0.5 same as above //
+        long int error; int PID;long int Kd_factor;
+        int speed;
+        long int LED1 = adc_read1();int LED2 = adc_read2();int LED3 = adc_read3();int LED4 = adc_read4();
+        int Sum_LED = LED1+LED2+LED3+LED4;
+        long int weighted_error = ((LED1)+(LED2*2)+(LED3*3)+(LED4*4))*1000;
+        error = (weighted_error/Sum_LED)-2500;
+        Kd_factor = (Kd *((error) - (prev_error)/100)); //This calculates the derivative side of the PD
+        PID = ((error*gain)+Kd_factor)/1000; // Calculates the PID value.
+        speed = corner_2(LED2,LED3);
+        if(prev_error == 30000){
+            speed=19;
+        }
+        if(PID>speed){
+            PID =speed;
+        }
+        if(PID<-speed){
+            PID = -speed;
+        }
+            OCR0A = (speed-PID); //this drives motor 2 or the left wheel
+            OCR1A =(speed+PID); //this drives motor 1 or the right wheel
+        _delay_ms(2); //Delay for the derivative function
+        return error;
 }
 
 
 
-void Emergency_brake() {
-    //This stops the robot if it runs off. This should work again now.
-    while (adc_read1() < 200 && adc_read2() < 50 && adc_read3() < 50 && adc_read4() < 200) {
+void check_position(){
+//This stops the robot if it runs off. This should work again now.
+    while(adc_read1()<50 && adc_read2()<50 && adc_read3()<50 && adc_read4()<50){ 
         OCR0A = 0;
         OCR1A = 0;
     }
 
 }
 
-
-
-
-
-int main() {
-    setup(); //Now includes a safety-pause and Christmas-tree style countdown.
-    int corner_count = 0;
-    int last_error;
-    int error;
-    while (1) {
-        Emergency_brake();
-        error = run_PID(last_error);
-        last_error = error;
+int Start_finish(int count){
+    int RHS = RHS_sidemarkerADC();
+    int LHS = LHS_sidemarkerADC();
+    if(RHS>200 && count<6 && LHS<100){
+        count++;
+    }else if(count<5){
+        count=0;
     }
-    return 0;
+    if (RHS<110 && count>=5 && count<=30 && LHS<100){
+        count++;
+    }
+    if(RHS>220&&count>=29&&LHS<100){
+        count++;
+    }
+    if (count>=38){
+        OCR0A = 0;
+        OCR1A = 0;
+        _delay_ms(2000);
+        count = 0;
+    }
+    return count;
+
+}
+
+int Corner_marker(int count){
+    int RHS = RHS_sidemarkerADC();
+    int LHS = LHS_sidemarkerADC();
+    if(LHS>150 && count<13 && RHS<150){
+        count++;
+        PORTB |=(1<<0);
+    }
+    if (LHS<110 && count>=12 && count<=30){
+        count++;
+        //PORTB = (PORTB & 0b11111110);
+    }
+    if(count>29){
+        count = 0;
+        PORTB = (PORTB & 0b11111110);
+    }
+    return count;
+
+}
+
+
+void main(){
+    setup();
+    _delay_ms(2000);
+        int corner_mem = 0;int corner_count=0; int corner=0;
+        int last_error;int error;int corner_on =0;int finish_count = 0;
+        while(1){
+            check_position();
+            error = run_PID(last_error);
+            last_error = error;
+            finish_count = Start_finish(finish_count);
+            corner_on = Corner_marker(corner_on);
+            if(corner_on==28){
+                corner++;
+            }
+
+            if(corner==15){
+                //PORTB |=(1<<0);
+                OCR0A = 0;
+                OCR1A = 0;
+                _delay_ms(15);
+                while(LHS_sidemarkerADC()<150){
+                    last_error=30000;
+                    int crap = run_PID(last_error);
+                }
+            }else{
+                //PORTB = (PORTB & 0b11111110);
+            }
+            if(finish_count ==0){
+                corner=0;
+                PORTB |=(1<<3);
+            }else{
+                PORTB = (PORTB & 0b11111011);
+            }
+            
+        }
 }
